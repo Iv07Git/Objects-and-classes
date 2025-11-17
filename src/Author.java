a162330ab66667ede2744367c73bd65bd329bb4b
Home <ivan.latyshev.2015@yandex.ru>
2025-11-17 08:01:08 +0800

    Add HW

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String family;
@@ -14,4 +16,26 @@ public class Author {
     public String getFamily () {
         return this.family;
     }
+
+        @Override
+        public String toString () {
+            return "Автор: " + this.name + " " + family;
+        }
+
+        @Override
+        public boolean equals (Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Author author = (Author) obj;
+            return Objects.equals(name, author.name) && Objects.equals(family, author.family);
+        }
+
+        @Override
+        public int hashCode () {
+            return Objects.hash(name, family);
+        }
 }
